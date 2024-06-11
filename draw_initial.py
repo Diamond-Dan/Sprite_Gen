1ebb5810e69c2b9a1f194a686fceb2bc3eef1785
Diamond-Dan <dreti.biz@gmail.com>
2024-06-10 19:08:35 -0700

    added color matching in draw_initial.py for parse_xml

--- draw_initial.py
+++ draw_initial.py
@@ -110,22 +110,22 @@ def parse_xml(xml_file_name, file_name):
             # print(partstitch.get('x'), partstitch.get('y'))
             x.append(int(partstitch.get("x")))
             y.append(int(partstitch.get("y")))
-            random_color_1 += random.randint(-1, 1)
-            random_color_2 += random.randint(-1, 1)
-            random_color_3 += random.randint(-1, 1)
-            random_color_4 += random.randint(-1, 1)
-            random_color_array.append(random_color_1)
-            random_color_array.append(random_color_2)
-            random_color_array.append(random_color_3)
-            random_color_array.append(random_color_4)
-            if random_color_1 <= 0 or random_color_1 >= 255:
-                random_color_1 = 255
-            if random_color_2 <= 0 or random_color_2 >= 255:
-                random_color_2 = 0
-            if random_color_3 <= 0 or random_color_3 >= 255:
-                random_color_3 = 0
-            if random_color_4 <= 0 or random_color_4 >= 255:
-                random_color_4 = 255
+            # random_color_1 += random.randint(-1, 1)
+            # random_color_2 += random.randint(-1, 1)
+            # random_color_3 += random.randint(-1, 1)
+            # random_color_4 += random.randint(-1, 1)
+            random_color_array.append(int(partstitch.get("color_1")))
+            random_color_array.append(int(partstitch.get("color_2")))
+            random_color_array.append(int(partstitch.get("color_3")))
+            random_color_array.append(255)
+            # if random_color_1 <= 0 or random_color_1 >= 255:
+            #     random_color_1 = 255
+            # if random_color_2 <= 0 or random_color_2 >= 255:
+            #     random_color_2 = 0
+            # if random_color_3 <= 0 or random_color_3 >= 255:
+            #     random_color_3 = 0
+            # if random_color_4 <= 0 or random_color_4 >= 255:
+            #     random_color_4 = 255
     # print(f'x: {x}, y: {y}')
     return x, y, random_color_array
 
