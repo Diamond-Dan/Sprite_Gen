3c847f364542b025a44bcde804e2de67cf34a140
Diamond-Dan <dreti.biz@gmail.com>
2024-06-21 13:51:34 -0700

    reformat using black to keep up with formatting

--- image_maker.py
+++ image_maker.py
@@ -7,6 +7,7 @@ import pygame
 import pygame_textinput
 import sprite_micro_gen
 
+
 class drawing_obj:
     def __init__(self, pos, color):
         self.pos = pos
@@ -17,6 +18,7 @@ class drawing_obj:
         self.color_2 = color[1]
         self.color_3 = color[2]
 
+
 def main():
     # Initialize Pygame
     pygame.init()
@@ -25,7 +27,7 @@ def main():
     saved = False
     # Set the width and height of the screen (width, height)
     size = (1200, 1000)  # draw on large grid which will later be scaled to 100x100
-    screen = pygame.display.set_mode(size)   
+    screen = pygame.display.set_mode(size)
     section = pygame.Surface((10, 1000))
     section.fill((100, 255, 255))
     section_background = pygame.Surface((190, 1000))
@@ -45,21 +47,20 @@ def main():
 
     # Create the root element for the XML document
     xml_root = ET.Element("root")
-    #fonts
+    # fonts
     font = pygame.font.Font(None, 28)
     pause_font = pygame.font.Font(None, 28)
-    #painting variables
+    # painting variables
 
     color = [255, 255, 255]
     paint_brush_size = 10
     paint_brush_font = font.render(str(paint_brush_size), True, (255, 0, 0))
     # Set initial positions for the inputs
     positions = [(10, 10), (10, 60), (10, 110), (10, 160), (10, 310)]
-   
+
     color_fonts = [0, 0, 0]
     for i, color_font in enumerate(color_fonts):
         color_fonts[i] = font.render(str(color[i]), True, (255, 0, 0))
-    
 
     preview_rect = pygame.Rect(10, 200, 50, 50)
     text_inputs = [pygame_textinput.TextInputVisualizer() for _ in range(3)]
@@ -70,7 +71,7 @@ def main():
 
     for i, text_input in enumerate(text_inputs):
         text_inputs[i].manager.input_string = color[i]
-    #strings
+    # strings
     pause_string = "Press Enter"
     pause_string_2 = "to change colors"
     paused_string = "DRAWING PAUSED"
@@ -78,7 +79,7 @@ def main():
     click_number_string = "click numbers to"
     click_number_string_2 = "assign RGB value"
     brush_string = "Brush Size"
-    #locations and loading for strings
+    # locations and loading for strings
     pause_text_location = (0, 360)
     paused_text_pos = (0, 260)
     paused_text_color = (255, 0, 0)
@@ -92,18 +93,16 @@ def main():
     click_num_2 = pause_font.render(click_number_string_2, True, paused_text_color)
     saved_text = pause_font.render(save_string, True, paused_text_color)
     brush_text = pause_font.render(brush_string, True, paused_text_color)
-    #background
+    # background
     background_color = (255, 255, 255)
     screen.fill(background_color)
-   
+
     menu = False
     rect_selected = -1
     grid_toggle = True
     create_images_button = pygame.Rect(5, 800, 160, 50)
     button_string = "Create Images"
     button_text = pause_font.render(button_string, True, (0, 0, 0))
-
-
 
     while not done:
 
@@ -117,7 +116,7 @@ def main():
 
                 screen.blit(section, (190, 0))
                 screen.blit(section_background, (0, 0))
-             
+
                 screen.blit(pause_text, pause_text_location)
                 screen.blit(pause_text_2, (0, 390))
                 screen.blit(click_num, (0, 420))
@@ -128,7 +127,13 @@ def main():
             screen.blit(color_fonts[2], positions[2])
             screen.blit(brush_text, brush_text_location)
             screen.blit(paint_brush_font, positions[4])
-            #draw buttons
+
+            # saving
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    # The 's' key was pressed! Show the save dialog.
+                    saved, filename = save_xml(xml_root)
+            # draw buttons
             pygame.draw.rect(screen, (0, 0, 0), create_images_button, 2)
             screen.blit(button_text, (10, 810))
             if event.type == pygame.QUIT:  # User clicked close
@@ -138,30 +143,38 @@ def main():
                     mouse_down = True
                     if event.pos[0] > 200 and event.pos[0] < 1200:
                         if event.pos[1] > 0 and event.pos[1] < 1000:
-                            drawning_saving_array.append(paint_on_canvas(color, paint_brush_size, event, xml_root, screen))
-                           
+                            drawning_saving_array.append(
+                                paint_on_canvas(
+                                    color, paint_brush_size, event, xml_root, screen
+                                )
+                            )
+
                 elif event.type == pygame.MOUSEMOTION and mouse_down:
                     # Draw at mouse position with right click down
                     if event.pos[0] > 200 and event.pos[0] < 1200:
                         if event.pos[1] > 0 and event.pos[1] < 1000:
-                           drawning_saving_array.append(paint_on_canvas(color, paint_brush_size, event, xml_root, screen))
+                            drawning_saving_array.append(
+                                paint_on_canvas(
+                                    color, paint_brush_size, event, xml_root, screen
+                                )
+                            )
 
-            
                 elif event.type == pygame.MOUSEBUTTONUP:  # Mouse button released
                     mouse_down = False
 
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_s:
-                        # The 's' key was pressed! Show the save dialog.
-                        saved, filename = save_xml(xml_root)
                     if event.key == pygame.K_g:
-                                                
+
                         grid_toggle = not grid_toggle
                         if not grid_toggle:
                             screen.fill((255, 255, 255, 0))
                             redraw(drawning_saving_array, screen)
-                    if event.key == pygame.K_z and (pygame.key.get_mods() & pygame.KMOD_CTRL):
-                        drawning_saving_array = undo(drawning_saving_array, xml_root, screen, background_color)
+                    if event.key == pygame.K_z and (
+                        pygame.key.get_mods() & pygame.KMOD_CTRL
+                    ):
+                        drawning_saving_array = undo(
+                            drawning_saving_array, xml_root, screen, background_color
+                        )
             # Update text inputs
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN and not menu:
@@ -170,6 +183,7 @@ def main():
                 elif event.key == pygame.K_RETURN and menu:
                     menu = False
             if menu:
+
                 pygame.draw.rect(screen, (0, 0, 0), text_rects[0], 2)
                 pygame.draw.rect(screen, (0, 0, 0), text_rects[1], 2)
                 pygame.draw.rect(screen, (0, 0, 0), text_rects[2], 2)
@@ -189,7 +203,19 @@ def main():
                         if filename is None:
                             saved, filename = save_xml(xml_root)
                         if filename is not None:
-                            sprite_micro_gen.main(500, 500, 25, 3, 1300, "2", 1, str(filename+".xml"), 15, str(filename+".xml"), False)
+                            sprite_micro_gen.main(
+                                500,
+                                500,
+                                25,
+                                3,
+                                1300,
+                                "2",
+                                1,
+                                str(filename + ".xml"),
+                                15,
+                                str(filename + ".xml"),
+                                False,
+                            )
             if rect_selected == 0:
 
                 pygame.draw.rect(screen, (255, 0, 0), text_rects[0], 2)
@@ -217,16 +243,17 @@ def main():
                 new_size = brush_size_select()
                 if new_size is not None:
                     paint_brush_size = new_size
-                    paint_brush_font = font.render(str(paint_brush_size), True, (255, 0, 0))
+                    paint_brush_font = font.render(
+                        str(paint_brush_size), True, (255, 0, 0)
+                    )
                     rect_selected = -1
-
 
         # Draw a preview of the pattern in the top left corner
         pygame.draw.rect(screen, (color[0], color[1], color[2]), preview_rect)
         # draw box around preview rect
         pygame.draw.rect(screen, (0, 0, 0), preview_rect, 2)
         #  Go ahead and update the screen with what we've drawn
-        
+
         if grid_toggle:
             grid = create_grid(screen)
             screen.blit(grid, (0, 0))
@@ -255,6 +282,7 @@ def main():
     # Close the window and quit
     pygame.quit()
 
+
 def undo(drawing_saving_array, xml_file, screen, background_color):
     print("undo")
     print(xml_file)
@@ -262,37 +290,37 @@ def undo(drawing_saving_array, xml_file, screen, background_color):
         return drawing_saving_array
     else:
         last_drawing = drawing_saving_array[-1]
-        pygame.draw.rect(screen, background_color, (last_drawing.x, last_drawing.y, 10, 10))
+        pygame.draw.rect(
+            screen, background_color, (last_drawing.x, last_drawing.y, 10, 10)
+        )
         drawing_saving_array.pop()
         xml_file.remove(xml_file[-1])
         return drawing_saving_array
-    
+
+
 def create_grid(screen):
     for x in range(200, 1200, 10):
         pygame.draw.line(screen, (0, 0, 0), (x, 0), (x, 1000))
     for y in range(0, 1000, 10):
         pygame.draw.line(screen, (0, 0, 0), (200, y), (1200, y))
     return screen
+
+
 def save_xml(xml_root):
     root = tk.Tk()
     root.withdraw()  # Hide the main window
     filename = None
     while filename is None or filename == "":
-        filename = simpledialog.askstring(
-            "Filename", "Enter a name for the XML file:"
-        )
-        
+        filename = simpledialog.askstring("Filename", "Enter a name for the XML file:")
+
         if filename is not None and filename != "":
             tree = ET.ElementTree(xml_root)
-            current_file_path = os.path.dirname(
-                os.path.realpath(__file__)
-            )
-            file_path = os.path.join(
-                current_file_path, "patterns", f"{filename}.xml"
-            )
+            current_file_path = os.path.dirname(os.path.realpath(__file__))
+            file_path = os.path.join(current_file_path, "patterns", f"{filename}.xml")
             tree.write(file_path)
             saved = True
     return saved, filename
+
 
 def color_select():
     root = tk.Tk()
@@ -300,11 +328,13 @@ def color_select():
     new_color = simpledialog.askinteger("Color", "Enter a new color (0-255):")
     return new_color
 
+
 def brush_size_select():
     root = tk.Tk()
     root.withdraw()  # Hide the main window
     new_size = simpledialog.askinteger("Brush Size", "Enter a new brush size (1-10):")
     return new_size
+
 
 def paint_on_canvas(color, size, event, xml_root, screen):
     # Draw at mouse position with right click down
@@ -321,7 +351,6 @@ def paint_on_canvas(color, size, event, xml_root, screen):
     postion = (event.pos[0], event.pos[1])
     new_drawing = drawing_obj(postion, color)
     return new_drawing
-
 
 
 def redraw(drawing_saving_array, screen):
